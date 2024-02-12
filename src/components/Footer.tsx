3ec3c7fd3fc01856414932d6f258710436d1db9e
Chrilleweb <christian.munknissen@gmail.com>
2024-02-12 20:56:07 +0100

    prettier

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,8 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <footer className="container fixed bottom-0 w-full bg-white text-gray-600 p-4 text-center">
-  <p className="text-sm">&copy; 2024 Chrilleweb All rights reserved.</p>
-</footer>
-
-
+      <p className="text-sm">&copy; 2024 Chrilleweb All rights reserved.</p>
+    </footer>
   );
 };
 
