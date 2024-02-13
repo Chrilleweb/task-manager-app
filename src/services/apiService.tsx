3055979f555e1b4890f1ac7f5beb02cfac790eef
Chrilleweb <christian.munknissen@gmail.com>
2024-02-14 00:03:44 +0100

    appservice simplifyed

--- src/services/apiService.tsx
+++ src/services/apiService.tsx
@@ -1,48 +1,37 @@
 interface ApiServiceProps {
-    url: string;
-    method: 'GET' | 'POST' | 'PUT' | 'DELETE';
-    body?: object | string;
-    includeToken?: boolean; // Optional parameter to include the token in the request
-  }
-  
-  const apiService = async ({ url, method, body, includeToken = true }: ApiServiceProps) => {
-    try {
-      const headers: Record<string, string> = {
-        'Content-Type': 'application/json',
-      };
-  
-      if (includeToken) {
-        const token = localStorage.getItem('token');
-        if (token) {
-          headers.Authorization = `Bearer ${token}`;
-        }
+  url: string;
+  method: "GET" | "POST" | "PUT" | "DELETE";
+  body?: object | string;
+}
+
+const apiService = async ({ url, method, body }: ApiServiceProps) => {
+  try {
+    const headers: Record<string, string> = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("token") || ""}`,
+    };
+
+    const response = await fetch(url, {
+      method,
+      headers,
+      body: method !== "GET" ? JSON.stringify(body) : undefined,
+    });
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      let errorMessage = "Request failed";
+      if (responseData && responseData.message) {
+        errorMessage = responseData.message;
       }
-  
-      const response = await fetch(url, {
-        method,
-        headers,
-        body: method !== 'GET' ? JSON.stringify(body) : undefined,
-      });
-  
-      const responseData = await response.json();
-  
-      if (!response.ok) {
-        let errorMessage = 'Request failed';
-        if (responseData && responseData.message) {
-          errorMessage = responseData.message;
-        }
-        throw new Error(errorMessage);
-      }
-  
-      return responseData;
-    } catch (error: any) {
-      console.error('API request failed:', error);
-      throw new Error(error.message || 'Internal Server Error');
+      throw new Error(errorMessage);
     }
-  };
-  
-  export default apiService;
-  
-  
-  
-  
+
+    return responseData;
+  } catch (error: any) {
+    console.error("API request failed:", error);
+    throw new Error(error.message || "Internal Server Error");
+  }
+};
+
+export default apiService;
